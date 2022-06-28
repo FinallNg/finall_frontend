f5a5385257f926ef0857b01799c87bbd58994bda
khelvyn80 <akyeaniv@gmail.com>
2022-06-28 23:59:10 +0000

    Add Navbar

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import NewsLetter from './NewsLetter/NewsLetter'
+import Navbar from '../Navbar/Navbar'
 
 function LandingPage() {
   return (
     <div>
+        <Navbar />
         <NewsLetter />
     </div>
   )
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,73 @@
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+function Navbar() {
+  return (
+    <header>
+        <div>
+            {/* <h2>Finall</h2> */}
+            <img src={logo} />
+        </div>
+
+        {/* <div className="nav-links">
+            <ul>
+               <Link to='/'> 
+                    <li>Home</li> 
+                </Link>
+
+                <Link to="/about">
+                    <li>
+                        About Us
+                    </li>
+                </Link>
+
+                <Link to="/contact">
+                    <li>
+                        Contact Us
+                    </li>
+                </Link>
+                
+                
+            </ul> 
+        </div> */}
+
+        {/* <input type="checkbox" /> */}
+
+
+        <div>
+            <ul>
+                <li>
+                    <a className='active' href='#'>Home</a>
+                </li>
+
+                <li>
+                    <a href='#'>About</a>
+                </li>
+
+                <li>
+                    <a href='#'>Services</a>
+                </li>
+
+                <li>
+                    <a href='#'>Pricing</a>
+                </li>
+
+                <li>
+                    <a href='#'>Contact Us</a>
+                </li>
+            </ul>
+
+        </div>
+
+        <div>
+            {/* <button>Login</button> */}
+
+            <a href='#' className='cta'>Login</a>
+
+            <button className='cta'>Get Started</button>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
