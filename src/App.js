e1126482077aa8c3793b9f04e75c99426c5bbfbf
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-06-28 19:02:13 +0100

    Subscribe to Newsletter section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import LandingPage from './Components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
-     <p>Finall Frontend</p>
+     {/* <p>Finall Frontend</p> */}
+     <LandingPage />
     </div>
   );
 }
