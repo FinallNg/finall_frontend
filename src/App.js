102fd9786f5795ca89798473607c00cd9af3dd61
Peter Uadiale <peteuadiale1@gmail.com>
2022-06-29 23:14:55 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import FinancialServices from "./Components/LandingPage/FinancialServices/FinancialServices";
-import Services from './Components/LandingPage/Services/Services';
 import Footer from "./Components/Footer/Footer"
 import './App.css';
 
@@ -7,9 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Services />
-     <FinancialServices />
-
     
      
   
