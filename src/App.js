3b2addf0c20b08293cbcc8b0dac771f5890e2ba8
fahadsarki <fahadsarki@gmail.com>
2022-07-01 22:24:21 +0100

    fixed app.js error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import './App.css';
-<<<<<<< HEAD
-import FeatureCover from './Components/LandingPage/FeatureCover/FeatureCover';
-=======
 import LandingPage from './Components/LandingPage/LandingPage';
 
 
->>>>>>> 6c1bad486ec9009b58f02716ad67c7c06fd40b4e
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-    <FeatureCover/>
-=======
       <LandingPage />
       
 
      {/* <p>Finall Frontend</p> */}
   
->>>>>>> 6c1bad486ec9009b58f02716ad67c7c06fd40b4e
     </div>
   );
 }
