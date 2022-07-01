ad71c32cb9a4dfe209ec6bfa7e5dfd8fa34528be
fahadsarki <fahadsarki@gmail.com>
2022-07-01 22:55:55 +0100

    landing page sorting

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -3,10 +3,11 @@ import HeroSection from './HeroSection/HeroSection'
 import SpendingHabit from './SpendingHabit/SpendingHabit'
 import NewsLetter from './NewsLetter/NewsLetter'
 import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
+// import Footer from '../Footer/Footer'
 import Team from './Team/Team'
 import FinancialServices from "../LandingPage/FinancialServices/FinancialServices"
 import Services from "../LandingPage/Services/Services"
+// import FeatureCover from './FeatureCover/FeatureCover'
 
 
 function LandingPage() {
@@ -14,11 +15,12 @@ function LandingPage() {
     <div>
         <Navbar />
         <HeroSection />
-        <SpendingHabit />
-        <NewsLetter />
-        <Team />
         <Services />
+        <SpendingHabit />
         <FinancialServices />
+        <Team />
+        {/* <FeatureCover/> */}
+        <NewsLetter />
         {/* <Footer /> */}
     </div>
   )
