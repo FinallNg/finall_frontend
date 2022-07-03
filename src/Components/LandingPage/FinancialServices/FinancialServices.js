8499f0806ca36707cc5b85737d1eba2421293fd8
Peter Uadiale <peteuadiale1@gmail.com>
2022-07-03 14:39:10 +0100

    update services.js

--- src/Components/LandingPage/FinancialServices/FinancialServices.js
+++ src/Components/LandingPage/FinancialServices/FinancialServices.js
@@ -4,23 +4,27 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 
 const FinancialServices = () => {
   return (
-    <div className="container">
-      <div className="col-left">
-        <div className="row-1">
-          <h3>Providing Smart Financial Services for Everyone</h3>
-        </div>
-        <div className="row-2">
-          <div className="btn-wrapper">
-            <button>Get Started</button>
+    <div className="financial-services">
+      <div className="container">
+        <div className="col-left">
+          <div className="row-1">
+            <h3>Providing Smart Financial Services for Everyone</h3>
           </div>
-          <a href="/">
-            <span>Our Services</span>
-            <AiOutlineArrowRight className="arrow-icon" />
-          </a>
+          <div className="row-2">
+            <div className="btn-wrapper">
+              <button>Get Started</button>
+            </div>
+            <a href="/">
+              <span>Our Services</span>
+              <AiOutlineArrowRight className="arrow-icon" />
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="col-right">
-        <img src={Finance} alt="financial service"/>
+        <div className="col-right">
+          <div className="img-container">
+            <img src={Finance} alt="financial service" />
+          </div>
+        </div>
       </div>
     </div>
   );
