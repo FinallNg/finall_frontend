d06391ec6f7c2224c8eedeaec50b2d8674abef6f
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-07-14 21:27:40 +0100

    Add sidebar

--- src/NewApp.js
+++ src/NewApp.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "./NewApp.css"
 
+import FeatureCover from "./Components/LandingPage/FeatureCover/FeatureCover";
 import ContactUs from "./Components/ContactUsPage/ContactUs";
 import LandingPage from "./Components/LandingPage/LandingPage";
 import Login from "./Components/Login/Login";
@@ -10,27 +12,26 @@ import SignUp from "./Components/SignUp/SignUp";
 import AboutUs from "./Components/AboutUs/AboutUs";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
+import Dashboard from "./Components/Dashboard/Dashboard";
 import Services from "./Components/Services/Services";
 
-// import Logout from "./Components/Dashboard/Logout/Logout"
+import CardsBanks from './Components/Dashboard/CardsBanks/CardsBanks';
+import Goals from './Components/Dashboard/Goals/Goals';
+import Help from './Components/Dashboard/Help/Help';
+import Invoices from './Components/Dashboard/Invoices/Invoices';
+import Logout from './Components/Dashboard/Logout/Logout';
+import Overview from './Components/Dashboard/Overview/Overview';
+import Settings from './Components/Dashboard/Settings/Settings';
+import Sidebar from './Components/Dashboard/Sidebar/Sidebar';
+import NewSidebar from './Components/Dashboard/Sidebar/NewSidebar';
+import Transactions from './Components/Dashboard/Transactions/Transactions'
 
-import Overview from "./Components/Dashboard/Overview/Overview";
-import Transactions from "./Components/Dashboard/Transactions/Transactions";
-import CardsBanks from "./Components/Dashboard/CardsBanks/CardsBanks";
-import Invoices from "./Components/Dashboard/Invoices/Invoices";
-import Goals from "./Components/Dashboard/Goals/Goals";
-import Settings from "./Components/Dashboard/Settings/Settings";
-import Help from "./Components/Dashboard/Help/Help";
-import Logout from "./Components/Dashboard/Logout/Logout";
-// import Sidebar from "./Components/Dashboard/Sidebar/Sidebar";
 
-
-function App() {
+function NewApp() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <div className="NewApp">
+      {/* <BrowserRouter>
         <Navbar />
-         {/* <Sidebar /> */}
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
           <Route exact path="/about" element={<AboutUs />} />
@@ -39,6 +40,15 @@ function App() {
           <Route exact path="/contact" element={<ContactUs />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter> */}
+
+      <BrowserRouter>
+        <Sidebar />
+        <Routes>
+        <Route exact path='/' element={ <Overview />} />
           <Route exact path='/overview' element={ <Overview />} />
           <Route exact path='/transactions' element={ <Transactions/>} />
           <Route exact path='/cards' element={<CardsBanks />} />
@@ -48,12 +58,12 @@ function App() {
           <Route exact path='/help' element={<Help />} />
           <Route exact path='/logout' element={<Logout />} />
         </Routes>
-        <Footer />
-        {/* <Logout /> */}
-      
       </BrowserRouter>
+
+      {/* <NewSidebar /> */}
+      {/* <Sidebar /> */}
     </div>
   );
 }
 
-export default App;
+export default NewApp;
