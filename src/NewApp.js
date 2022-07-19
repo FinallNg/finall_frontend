17cf6d8a4ce6b4f237747bd6171b82058e53dad1
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-07-19 17:13:50 +0100

    fix metrics responsiveness

--- src/NewApp.js
+++ src/NewApp.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import "./NewApp.css"
 
 import CardsBanks from './Components/Dashboard/CardsBanks/CardsBanks';
