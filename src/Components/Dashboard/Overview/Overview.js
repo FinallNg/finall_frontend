499d3598745c576e2217a5333eb6c9fad218c3ff
josiah-williams <josiahdamiwilliams@gmail.com>
2022-08-29 22:26:08 +0100

    finished consuming api for totalbalance

--- src/Components/Dashboard/Overview/Overview.js
+++ src/Components/Dashboard/Overview/Overview.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from "react"
 import { CounterWidget, ExpenseWidget, SavingsWidget } from './widgets';
 import "../Overview/Overview.css";
 import MiniCalendar from './Minicalendar';
@@ -6,17 +6,50 @@ import {ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend}
 import { data } from './data';
 import Tabledata from './components/Tabledata';
 import Doughnut from './doughnut';
-
+import axios from 'axios';
+import { Card } from '@themesberg/react-bootstrap';
+import { BiUpArrowAlt, BiDownArrowAlt} from"react-icons/bi"
+import { NavItem } from "react-bootstrap";
 
 export default function Overview(){
+  const [title, setTitle] = useState([]);
+useEffect(() => {
+  fetchbalance();
+},[]);
+  const fetchbalance = () => {
+    axios
+        .get('https://finall-app.herokuapp.com/api/v1/users/62b208fab5e09b628baa2429/accounts/totalBalance')
+        .then((res) => {
+      console.log(res);
+      setTitle(res.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
   return (
     <div className='OverviewBox'>
+      
       <div className='first-box'>
-        <CounterWidget 
-        category ='Balance'
-        title = '$27632'
-        percentage = {+2.5}
-        />
+     <Card border="light" className="shadow-sm">
+            <div className="category-text">
+
+              <h5>Balance</h5>
+              <h3 className="mb-1">${title.totalBalance}</h3>
+            </div>
+              
+            <div className="small mt-2">
+              <span>
+                +%
+                <BiUpArrowAlt className="up-arrow"/>
+              </span>
+            </div>
+            <div className="lastly">
+            <p>Compared to($21340 last year)</p>
+            </div>
+            
+      </Card>
+        
         <ExpenseWidget 
           category ='Expenses'
           title = '$20199'
