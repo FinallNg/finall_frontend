f2a6109d68cd744925b8f404c9dd6c10d3cd422e
fahadsarki <fahadsarki@gmail.com>
2022-08-29 20:28:08 +0100

    mine

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroSection from './HeroSection/HeroSection'
 import SpendingHabit from './SpendingHabit/SpendingHabit'
 import NewsLetter from './NewsLetter/NewsLetter'
-import Team from './Team/Team'
+// import Team from './Team/Team'
 import FinancialServices from "../LandingPage/FinancialServices/FinancialServices"
 import Services from "../LandingPage/Services/Services"
 import FeatureCover from './FeatureCover/FeatureCover'
@@ -21,7 +21,6 @@ function LandingPage() {
         <FinancialServices />
         <Overview />
         <FeatureCover />
-        {/*<Team />*/}
         <NewsLetter />
 
     </div>
