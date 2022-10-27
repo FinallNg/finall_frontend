1ccd8a0f104c146902764c95f9177ea4c10b0890
Only-Samuel <68018429+sammy867@users.noreply.github.com>
2022-10-27 04:35:27 +0100

    keep at it

--- src/Components/Dashboard/ConnectBank/ConnectBank.js
+++ src/Components/Dashboard/ConnectBank/ConnectBank.js
@@ -2,16 +2,7 @@ import React from 'react'
 import './ConnectBank.css'
 import Dot from '../../../assets/dot.png'
 import Bell from '../../../assets/bell.png'
-import Open from '../../../assets/Open.png'
-import Bar from '../../../assets/bar.png'
-import Bars from '../../../assets/bars.png'
-import Arrow from '../../../assets/arrow.png'
-import Analyse from '../../../assets/analyse.png'
-import Money from '../../../assets/money.png'
-import Kite from '../../../assets/kite.png'
-import Kites from '../../../assets/kites.png'
-import Square from '../../../assets/square.png'
-import Bulb from '../../../assets/bulb.png'
+import Door from '../../../assets/open_door.png'
 import Profile from '../../../assets/Ellipse.png'
 import Message from '../../../assets/email.png'
 import LinkAccount from '../../LinkAccount/LinkAccount'
@@ -32,16 +23,7 @@ const ConnectBank = () => {
         <img src = {Dot} alt = '' />
         <img src = {Message} alt = '' />
         <img src = {Profile} alt = '' />
-        <img src = {Open} alt = '' />
-        <img src = {Bar} alt = '' />
-        <img src = {Bars} alt = '' />
-        <img src = {Analyse} alt = '' />
-        <img src = {Arrow} alt = '' />
-        <img src = {Money} alt = '' />
-        <img src = {Bulb} alt = '' />
-        <img src = {Square} alt = '' />
-        <img src = {Kite} alt = '' />
-        <img src = {Kites} alt = '' />
+        <img src = {Door} alt = '' />
         <span>Welcome to</span>
         <span>Finall!</span>
         <span>Get</span>
